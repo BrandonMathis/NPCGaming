3e033f619f0a138e399d1a55d3b79747b0ecbad2
BrandonMathis <Brandon@KeysetTS.com>
2011-12-18 16:43:34 -0500

    Assign authors to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_filter :login_required, :except => [:index, :show]
+
   def index
     @posts = Post.all
   end
@@ -13,6 +15,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(params[:post])
+    @post.user = current_user
     if @post.save
       redirect_to @post, :notice => "Successfully created post."
     else
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :body
 
   belongs_to :user
+
   alias :author :user
 end
