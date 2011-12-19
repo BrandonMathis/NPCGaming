7c8fbe0e75bf2effa65a6b9cd268cc085b849a01
BrandonMathis <Brandon@KeysetTS.com>
2011-12-19 14:43:43 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create( :username => "admin", :password => 'password', :password_confirmation => 'password', :email => 'admin@example.com')
