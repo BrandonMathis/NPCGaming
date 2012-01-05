d1db3577e91683dabb6665d20744d9962c2bfbf4
BrandonMathis <Brandon@KeysetTS.com>
2012-01-05 16:39:00 -0500

    Order posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :login_required, :except => [:index, :show]
 
   def index
-    @posts = Post.all
+    @posts = Post.all :order => "created_at DESC"
   end
 
   def show
