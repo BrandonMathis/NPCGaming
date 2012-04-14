29650ab5366267039452548d69f3bd5086a5ea96
BrandonMathis <Brandon@KeysetTS.com>
2012-04-14 04:59:49 -0400

    Clean up About Us page and new user form

--- app/controllers/content/page_controller.rb
+++ app/controllers/content/page_controller.rb
@@ -1,7 +1,15 @@
 class Content::PageController < ApplicationController
+  helper_method :authors
+
   def about_us
+    @authors_list = authors
     respond_to do |format|
       format.html
     end
+  end
+
+  private
+  def authors
+    User.all
   end
 end
