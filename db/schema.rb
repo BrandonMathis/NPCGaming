b888ce1c8a438ef42625ea560d21a273dcece114
BrandonMathis <Brandon@KeysetTS.com>
2012-04-14 03:49:17 -0400

    Fixed type of text column in post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120120041816) do
+ActiveRecord::Schema.define(:version => 20120414074704) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(:version => 20120120041816) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body",        :limit => 255
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.string   "parsed_body"
     t.integer  "category_id"
   end
